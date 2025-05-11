53d5ba881b8add7d236c1ff8162935f65fdc6d7b
FlekGeKei <FlekGeKei@outlook.com>
2025-05-11 18:43:09 +0200

    cli stuff

--- src/core.rs
+++ src/core.rs
@@ -14,93 +14,98 @@ pub struct Recipe {
     craft_time: f64,
     craft_amount: f64,
 }
+impl Recipe {
+    pub fn get_recipes(path: &Path) -> Result<HashMap<String, Self>, std::io::Error> {
+        let mut hash_map: HashMap<String, Recipe> = Default::default();
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        let json_recep: Vec<Recipe> = serde_json::from_reader(reader)?;
+        for rec in json_recep {
+            hash_map.insert(rec.name.clone(), rec);
+        }
+
+        Ok(hash_map)
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[allow(dead_code)]
 pub struct Ingredient {
-    name: String,
-    amount: f64,
+    pub name: String,
+    pub amount: f64,
+}
+
+impl Ingredient {
+    pub fn get_request(path: &Path) -> Result<Vec<Self>, std::io::Error> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        let json_request: Vec<Ingredient> = serde_json::from_reader(reader)?;
+
+        Ok(json_request)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[allow(dead_code)]
 pub struct Instruction {
-    ingredient: Ingredient,
-    sub_instruction: Option<Vec<Instruction>>,
+    pub ingredient: Ingredient,
+    pub sub_instruction: Option<Vec<Instruction>>,
+}
+
+impl Instruction {
+    pub fn get_instruction(hash_map: &HashMap<String, Recipe>, order: Ingredient) -> Self {
+        let sub_instructions: Option<Vec<Instruction>>;
+
+        match hash_map.get(&order.name) {
+            Some(order_recipe) => {
+                let order_production_per_second =
+                    order.amount / order_recipe.craft_amount / order_recipe.craft_time;
+
+                match &order_recipe.ingredients {
+                    Some(order_recipe_ingrs) => {
+                        let mut tmp_instructions: Vec<Instruction> = Vec::new();
+
+                        for order_recipe_ingr_to_order in order_recipe_ingrs {
+                            let neded_amount =
+                                order_recipe_ingr_to_order.amount * order_production_per_second;
+
+                            let instruction_out = Instruction::get_instruction(
+                                hash_map,
+                                Ingredient {
+                                    name: order_recipe_ingr_to_order.name.clone(),
+                                    amount: neded_amount,
+                                },
+                            );
+                            tmp_instructions.push(instruction_out);
+                        }
+
+                        sub_instructions = Some(tmp_instructions);
+                    }
+                    None => sub_instructions = None,
+                }
+            }
+            None => {
+                panic!(
+                    "ERROR: trying to found nonexisting \"{}\" in {hash_map:?}",
+                    &order.name
+                );
+            }
+        }
+
+        Instruction {
+            ingredient: Ingredient {
+                name: order.name.clone(),
+                amount: order.amount,
+            },
+            sub_instruction: sub_instructions,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum RecipeKind {
     Complex(String),
     Simple,
-}
-
-pub fn get_recipes(path: &Path) -> Result<HashMap<String, Recipe>, std::io::Error> {
-    let mut hash_map: HashMap<String, Recipe> = Default::default();
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    let json_recep: Vec<Recipe> = serde_json::from_reader(reader)?;
-    for rec in json_recep {
-        hash_map.insert(rec.name.clone(), rec);
-    }
-
-    Ok(hash_map)
-}
-
-pub fn get_order(path: &Path) -> Result<Vec<Ingredient>, std::io::Error> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    let json_orders: Vec<Ingredient> = serde_json::from_reader(reader)?;
-
-    Ok(json_orders)
-}
-
-pub fn get_instruction(hash_map: &HashMap<String, Recipe>, order: Ingredient) -> Instruction {
-    let sub_instructions: Option<Vec<Instruction>>;
-
-    match hash_map.get(&order.name) {
-        Some(order_recipe) => {
-            let order_production_per_second =
-                order.amount / order_recipe.craft_amount / order_recipe.craft_time;
-
-            match &order_recipe.ingredients {
-                Some(order_recipe_ingrs) => {
-                    let mut tmp_instructions: Vec<Instruction> = Vec::new();
-
-                    for order_recipe_ingr_to_order in order_recipe_ingrs {
-                        let neded_amount =
-                            order_recipe_ingr_to_order.amount * order_production_per_second;
-
-                        let instruction_out = get_instruction(
-                            hash_map,
-                            Ingredient {
-                                name: order_recipe_ingr_to_order.name.clone(),
-                                amount: neded_amount,
-                            },
-                        );
-                        tmp_instructions.push(instruction_out);
-                    }
-
-                    sub_instructions = Some(tmp_instructions);
-                }
-                None => sub_instructions = None,
-            }
-        }
-        None => {
-            panic!(
-                "ERROR: trying to found nonexisting \"{}\" in {hash_map:?}",
-                &order.name
-            );
-        }
-    }
-
-    Instruction {
-        ingredient: Ingredient {
-            name: order.name.clone(),
-            amount: order.amount,
-        },
-        sub_instruction: sub_instructions,
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,83 @@
-mod cli;
 mod core;
 
-use crate::core::get_instruction;
-use crate::core::get_order;
-use crate::core::get_recipes;
+use core::Ingredient;
 use core::Instruction;
-use std::env;
-use std::path::Path;
+use core::Recipe;
+use std::path::PathBuf;
+
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// Help to do
+    #[arg(long)]
+    recipes: Option<PathBuf>,
+
+    /// Help to do
+    #[arg(long)]
+    request: Option<PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// help to do
+    Cmd,
+    /// help to do
+    Cli {
+        /// Help to do
+        #[arg(long)]
+        recipes: PathBuf,
+
+        /// Help to do
+        #[arg(long)]
+        request: PathBuf,
+    },
+}
+
+impl Commands {
+    fn march(commands: Commands) -> Vec<Instruction> {
+        match commands {
+            Commands::Cmd => panic!("CMD command is WIP"),
+            Commands::Cli { recipes, request } => Cli::run(recipes, request),
+        }
+    }
+}
+
+impl Cli {
+    fn run(recipes_path: PathBuf, request_path: PathBuf) -> Vec<Instruction> {
+        let recipes = Recipe::get_recipes(&recipes_path).expect("normal json");
+
+        let mut instructions: Vec<Instruction> = Vec::new();
+        for order in Ingredient::get_request(&request_path).expect("normal json") {
+            instructions.push(Instruction::get_instruction(&recipes, order));
+        }
+
+        instructions
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    let recipes = get_recipes(Path::new("./recipes.json")).expect("normal json");
+    let recipes_path = match cli.recipes {
+        Some(path) => path,
+        None => PathBuf::from("./recipes.json"),
+    };
 
-    let mut instructions: Vec<Instruction> = Vec::new();
-    for order in get_order(Path::new("./request.json")).expect("normal json") {
-        instructions.push(get_instruction(&recipes, order));
-    }
+    let request_path = match cli.request {
+        Some(path) => path,
+        None => PathBuf::from("./request.json"),
+    };
+
+    let instructions = match cli.command {
+        Some(commands) => Commands::march(commands),
+        None => Cli::run(recipes_path, request_path),
+    };
+
     dbg!(instructions);
 }
